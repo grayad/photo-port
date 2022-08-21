aa822c8b66070ed28a5e8629e15974c192fbbb01
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-21 15:33:48 -0400

    write tests for Nav component

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
+
+// will ensure that after each test, we won't have any leftover memory data that could give us false results
+afterEach(cleanup);
+
+describe("Nav component", () => {
+  // First Test- baseline test to verify component is rendering
+  it("renders", () => {
+    render(<Nav />);
+  });
+  //   Second Test- test case to compare versions of the DOM node structure
+  it("matches snapshot DOM node structure", () => {
+    // render Nav
+    const { asFragment } = render(<Nav />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+//   test to ensure emoji is visible
+describe("emoji is visible", () => {
+  it("inserts emoji into the h2", () => {
+    // Arrange
+    const { getByLabelText } = render(<Nav />);
+    // Assert
+    expect(getByLabelText("camera")).toHaveTextContent("📸");
+  });
+});
+
+// test to ensure links in Nav are visible
+describe("links are visible", () => {
+  it("inserts text into the links", () => {
+    // Arrange
+    const { getByTestId } = render(<Nav />);
+    // Assert
+    expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
+    expect(getByTestId("about")).toHaveTextContent("About me");
+  });
+});
