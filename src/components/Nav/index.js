56e671f9c986a779602c4a304950aaadb96798d6
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-25 14:32:02 -0400

    create useState in App.js for categories and use as props in Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,27 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-  // onclick function
-  function categorySelected(name) {
-    console.log(`${name} clicked`);
-  }
-  // array of photo categories
-  const categories = [
-    {
-      name: "Commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "Portraits", description: "Portraits of people in my life" },
-    { name: "Food", description: "Delicious delicacies" },
-    {
-      name: "Landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
+function Nav(props) {
+  const {
+    categories=[],
+    setCurrentCategory,
+    currentCategory
+  } = props;
+
   //   return JSX
   return (
-    <header>
+    <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
@@ -34,16 +23,29 @@ function Nav() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">About me</a>
+            <a data-testid="about" href="#about">
+              About me
+            </a>
           </li>
           <li>
             <span>Contact</span>
           </li>
           {/* map through cateories array and create nav list item for each */}
           {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && "navActive"
+              }`}
+              key={category.name}
+            >
               {/* add event listener and category name JSX */}
-              <span onClick={categorySelected}>{category.name}</span>
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
             </li>
           ))}
         </ul>
